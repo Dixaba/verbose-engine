4cedee09941cb089a83d4e9b17f9a1607cdc83a7
Dixaba <dixaba@yandex.ru>
2020-04-26 05:03:21 +0300

    Will it work

--- build.sh
+++ build.sh
@@ -8,10 +8,9 @@ then
   FILENAME=$(sed -e 's/\-/\./g; s/latest\.\?//; s/$/\.Dockerfile/; s/^\.//' <<< $NAME)
   if [ -e $FILENAME ];
   then
-    #docker build -f $FILENAME -t $REPONAME:$NAME .
-    echo $FILENAME
-    # VERSION=$(grep -oP 'Using Qt version \K[0-9.]+' <<< $(docker run --rm $REPONAME:$NAME qmake --version))
-    # docker image tag $REPONAME:$NAME $REPONAME:$(sed "s/latest/$VERSION/" <<< $NAME)
+    docker build -f $FILENAME -t $REPONAME:$NAME .
+    VERSION=$(grep -oP 'Using Qt version \K[0-9.]+' <<< $(docker run --rm $REPONAME:$NAME qmake --version))
+    docker image tag $REPONAME:$NAME $REPONAME:$(sed "s/latest/$VERSION/" <<< $NAME)
   else
     echo "No such Dockerfile found!";
   fi
